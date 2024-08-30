fe11da1b9515f81e9805f809b3c3cff005533f94
Anirudh Gokamalla <113572398+anirudhviswanath2001@users.noreply.github.com>
2024-08-30 12:00:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,8 +56,12 @@ X_test, Y_test=create_dataset(test_data,time_step)
 X_train=X_train.reshape(X_train.shape[0],X_train.shape[1],1)
 X_test=X_test.reshape(X_test.shape[0],X_test.shape[1],1) 
 
+@st.cache_resource
+def load_model():
+    return tensorflow.keras.models.load_model('StockModel.h5')
 
-model = load_model('StockModel.h5')
+model = load_model()
+
 
 train_predict=model.predict(X_train)
 test_predict=model.predict(X_test)
