999502a3c018b495dfd750fa0ee567bef375e2eb
Anirudh Gokamalla <113572398+anirudhviswanath2001@users.noreply.github.com>
2024-08-31 16:39:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import streamlit as st
 st.title('Stock Market Prediction for Short Term Investments')
 user_input=st.text_input('Stock Ticker','AAPL')
 st.subheader('Data for the last 60 days')
-date_start = st.text_input('YYYY-MM-DD', '2024-08-31')  # Wrap date in quotes
+date_start = st.text_input('YYYY-MM-DD', '2024-08-31')  
 date_end = st.text_input('YYYY-MM-DD', '2024-08-29')    
 data=yf.download(user_input,date_start,date_end,interval='2m')
 
