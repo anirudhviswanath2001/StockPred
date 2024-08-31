c52546e5c1b515cce5f81749167d19b57d1ee004
Anirudh Gokamalla <113572398+anirudhviswanath2001@users.noreply.github.com>
2024-08-31 16:36:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ import streamlit as st
 
 st.title('Stock Market Prediction for Short Term Investments')
 user_input=st.text_input('Stock Ticker','AAPL')
-date_start=st.text_input('YYYY-MM-DD','2024-07-01')
-date_end=st.text_input('YYYY-MM-DD','2024-08-29')
+st.subheader('Data for the last 60 days')
+date_start=st.text_input('YYYY-MM-DD')
+date_end=st.text_input('YYYY-MM-DD')
 data=yf.download(user_input,date_start,date_end,interval='2m')
 
 #Describing the data
-st.subheader('Data for the last 60 days')
+
 # Display summary statistics with custom size
 st.dataframe(data.describe(), width=700, height=300)
 st.subheader('Closing Price vs Time Chart')
